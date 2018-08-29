e2f87ccc6192a345cb7fdbaae2fe6832cbc808fe
SeanRLockart <39708478+SeanRLockart@users.noreply.github.com>
2018-08-29 16:11:28 -0400

    Add files via upload

---
+++ chargeLazar.java
@@ -0,0 +1,16 @@
+public class Main{
+	
+	static void chargeLazar(int charge){
+		
+		if (test <= 100){
+			System.out.println(charge);
+			println(charge + 5);
+			System.out.println(charge);
+		}
+	}
+	public static void main(String[] args){
+		
+		int recusionTest = 5;
+		chargeLazar(recursionTest);
+	}
+}
---
+++ playerInfo.java
@@ -0,0 +1,17 @@
+public class Main {
+	
+	
+	static double playerInfo (String place)
+	{
+		System.out.println("bonjourno player, you are in quadrant " + place);
+	}
+	static double playerInfo (int subSection)
+	{
+		System.out.println ("You are in Sub-Section"+ place + subSection );
+	}
+	public static void maine (String[] args)
+	{
+		playerInfo ("a");
+		playerInfo (28834);
+	}
+}
